32802029afb28c1875a540e3b0666d714c55717f
lhuissier4 <lhurem20@gmail.com>
2026-02-10 10:56:22 +0100

    fix syntax

--- migrations/up/01_create_table_players.sql
+++ migrations/up/01_create_table_players.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS PLAYERS (
     id    BIGINT PRIMARY KEY,
     dateBirth  VARCHAR,
-    dateRegistration   TIMESTAMP
+    dateRegistration   TIMESTAMP,
     country VARCHAR(5)
 );
