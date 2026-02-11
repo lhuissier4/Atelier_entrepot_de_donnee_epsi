6f727d6e75d5ed4f795231c1457ca94d8661500d
lhuissier4 <lhurem20@gmail.com>
2026-02-11 08:16:44 +0100

    add columns

--- migrations/up/05_create_table_players_medalha.sql
+++ migrations/up/05_create_table_players_medalha.sql
@@ -1,7 +1,11 @@
 CREATE TABLE IF NOT EXISTS PLAYERS_MEDALHA (
+    id BIGINT PRIMARY KEY,
     idPlayer   BIGINT NOT NULL,
     idMedal    BIGINT NOT NULL,
-    earnedAt   TIMESTAMP,
+    dtCreatedAt TIMESTAMP NOT NULL,
+    dtExpiration TIMESTAMP NOT NULL,
+    dtRemove TIMESTAMP NOT NULL,
+    flActive BOOLEAN NOT NULL,
 
     CONSTRAINT pk_players_medals
         PRIMARY KEY (idPlayer, idMedal),
